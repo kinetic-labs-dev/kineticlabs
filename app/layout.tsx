8245c558fb35264b66c50139e1dcc17619b541af
Alvaro <ageminiprox002@gmail.com>
2026-03-12 08:35:39 +0100

    Bloqueo nuclear de scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,13 +26,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={`${geistSans.variable} ${geistMono.variable}`} style={{ overflowX: "hidden" }}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`} style={{ margin: 0, padding: 0, background: "#050505" }}>
         
-        <Header />
-
-        {children}
-
-        <Footer />
+        {/* LA CAJA FUERTE: Esto fuerza a que NADA pueda salir por los lados */}
+        <div style={{ overflowX: "hidden", width: "100%", position: "relative", maxWidth: "100vw" }}>
+          <Header />
+          {children}
+          <Footer />
+        </div>
 
       </body>
     </html>
